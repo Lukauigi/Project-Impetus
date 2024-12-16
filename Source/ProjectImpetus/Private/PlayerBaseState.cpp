f1d6038ed85f9148502213f2eff7d3b0cf2e25b7
Lukauigi <lukebednarek@hotmail.ca>
2024-12-16 00:53:59 -0800

    Comment out log

--- Source/ProjectImpetus/Private/PlayerBaseState.cpp
+++ Source/ProjectImpetus/Private/PlayerBaseState.cpp
@@ -18,7 +18,7 @@ void UPlayerBaseState::UpdateState(float DeltaTime)
 {
     /*if (m_InputSubSystem) UE_LOG(LogTemp, Log, TEXT("SubSystem exists"));
     if (m_IMC) UE_LOG(LogTemp, Log, TEXT("IMC exists"));*/
-    if (m_EnhancedInputComponent) UE_LOG(LogTemp, Log, TEXT("Input Comp exists"));
+    //if (m_EnhancedInputComponent) UE_LOG(LogTemp, Log, TEXT("Input Comp exists"));
 }
 
 void UPlayerBaseState::InitState(UEnhancedInputLocalPlayerSubsystem* InputSubsystem, UEnhancedInputComponent* EnhancedInputComponent)
