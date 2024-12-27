cf05b7ca1ffc7e35f0f39ba88122f0d0d961c0d1
Lukauigi <lukebednarek@hotmail.ca>
2024-12-26 18:26:19 -0800

    Add func call from IMC mapping & IA_GrappleTether triggers once on press

--- Source/ProjectImpetus/Private/PlayerRoamingState.cpp
+++ Source/ProjectImpetus/Private/PlayerRoamingState.cpp
@@ -2,12 +2,24 @@
 
 
 #include "PlayerRoamingState.h"
+#include "../../../../../../../../Program Files/Epic Games/UE_5.3/Engine/Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h"
+#include "../../../../../../../../Program Files/Epic Games/UE_5.3/Engine/Plugins/EnhancedInput/Source/EnhancedInput/Public/InputMappingContext.h"
 
 void UPlayerRoamingState::EnterState()
 {
 	Super::EnterState();
-
 	UE_LOG(LogTemp, Log, TEXT("Entering Roaming State"));
+
+	const TArray<FEnhancedActionKeyMapping>& Mappings = m_IMC->GetMappings();
+	for (const FEnhancedActionKeyMapping& Mapping : Mappings)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Mapping: Action %s, Key %s"), *Mapping.Action->GetName(), *Mapping.Key.ToString());
+		if (Mapping.Action->GetFName() == "IA_GrappleTether")
+		{
+
+			m_EnhancedInputComponent->BindAction(Mapping.Action, ETriggerEvent::Triggered, this, &UPlayerRoamingState::Tether);
+		}
+	}
 }
 
 void UPlayerRoamingState::ExitState()
@@ -22,4 +34,9 @@ void UPlayerRoamingState::UpdateState(float DeltaTime)
 	Super::UpdateState(DeltaTime);
 
 	UE_LOG(LogTemp, Log, TEXT("Updating Roaming State"));
+}
+
+void UPlayerRoamingState::Tether(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Log, TEXT("Tether func called"));
 }
