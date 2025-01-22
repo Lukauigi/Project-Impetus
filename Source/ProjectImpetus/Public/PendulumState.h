32ff0ae0f2704260a0fd895a127ec3e3706bbd93
Lukauigi <lukebednarek@hotmail.ca>
2025-01-22 11:59:48 -0800

    Level & BP setup for pendulum test

---
+++ Source/ProjectImpetus/Public/PendulumState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerBaseState.h"
+#include "PendulumState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTIMPETUS_API UPendulumState : public UPlayerBaseState
+{
+	GENERATED_BODY()
+	
+};
